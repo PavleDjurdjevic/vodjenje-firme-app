cdd1abc0f94757811830119e1a41eee1a70364f2
PavleDjurdjevic <pavledjurdjevic14@gmail.com>
2026-02-19 17:41:35 +0100

    ssl & connectTimeout

--- backend/db.js
+++ backend/db.js
@@ -1,19 +1,22 @@
-const mysql = require('mysql2');
+const mysql = require("mysql2");
 
 const connection = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: Number(process.env.DB_PORT)
+  port: Number(process.env.DB_PORT),
+  ssl: { rejectUnauthorized: false },
+  connectTimeout: 10000,
 });
 
 connection.connect((err) => {
   if (err) {
-    console.error('Greška pri povezivanju sa bazom:', err);
+    console.error("Greška pri povezivanju sa bazom:", err);
   } else {
-    console.log('Uspesna konekcija sa bazom vodjenje_firme');
+    console.log("Uspešna konekcija sa bazom");
   }
 });
 
 module.exports = connection;
+
