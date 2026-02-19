b12b93c862c266ce4f75f8e35b6c958fa0a8ef13
PavleDjurdjevic <pavledjurdjevic14@gmail.com>
2026-02-19 18:41:55 +0100

    Insert MYSQL_PUBLIC_URL

--- backend/db.js
+++ backend/db.js
@@ -1,20 +1,14 @@
 const mysql = require("mysql2");
 
-const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: Number(process.env.DB_PORT),
-  ssl: { rejectUnauthorized: false },
-  connectTimeout: 10000,
-});
+const url = process.env.MYSQL_PUBLIC_URL; // <-- PUBLIC, ne MYSQL_URL
+
+const connection = mysql.createConnection(url);
 
 connection.connect((err) => {
   if (err) {
     console.error("Greška pri povezivanju sa bazom:", err);
   } else {
-    console.log("Uspešna konekcija sa bazom");
+    console.log("Uspešno povezano sa Railway bazom!");
   }
 });
 
