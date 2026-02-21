ff0f6d7cbe026dbefef941b26ffd795fab0facc3
PavleDjurdjevic <pavledjurdjevic14@gmail.com>
2026-02-21 14:51:39 +0100

    Add env example

--- backend/app.js
+++ backend/app.js
@@ -14,8 +14,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(cors({
   origin: [
-    "http://localhost:5173",
-    "https://vodjenje-firme-app.onrender.com"
+    "http://localhost:5173"
   ],
   credentials: true
 }));
